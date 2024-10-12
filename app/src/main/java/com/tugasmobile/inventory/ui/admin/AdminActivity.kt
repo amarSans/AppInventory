08b4b910f393ae65dc9aa40cd5894e5c89e7f7e0
amarSans <amuammar698@gmail.com>
2024-10-12 20:48:59 +0800

    memperbaiki desain tampilan appbar admin

--- app/src/main/java/com/tugasmobile/inventory/ui/admin/AdminActivity.kt
+++ app/src/main/java/com/tugasmobile/inventory/ui/admin/AdminActivity.kt
@@ -2,8 +2,11 @@ package com.tugasmobile.inventory.ui.admin
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.tugasmobile.inventory.MainActivity
+import com.tugasmobile.inventory.R
 import com.tugasmobile.inventory.databinding.ActivityAdminBinding
 import com.tugasmobile.inventory.ui.uiData.addData
 
@@ -18,6 +21,15 @@ class AdminActivity : AppCompatActivity() {
             val intent= Intent(this, addData::class.java)
             startActivity(intent)
         }
+        binding.iamgadmin.setOnClickListener{
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 }
